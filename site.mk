629cd724098ebbc44dbfcda20e613c0c5a9497dc
Marcel Pennewiss <github@pennewiss.de>
2016-09-16 21:30:04 +0200

    Update to 1.2.1 stable release

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 1.2.0-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 1.2.1
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
