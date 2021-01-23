104d125fcf6a55212873d973f6ed56f6750d5b59
hipposen <hipposen@users.noreply.github.com>
2021-01-23 10:52:18 +0100

    Update run-build.sh

--- contrib/actions/run-build.sh
+++ contrib/actions/run-build.sh
@@ -9,7 +9,9 @@ export GLUON_SITEDIR="../"
 export GLUON_TARGET=$1
 export BUILD_LOG=1
 
+echo $GLUON_TARGET
+
 make -C gluon update GLUON_SITEDIR="$GLUON_SITEDIR" V=1
 
-make -C gluon GLUON_TARGET=$1 GLUON_BRANCH=nightly GLUON_SITEDIR="$GLUON_SITEDIR" V=1 -j`nproc`
+make -C gluon GLUON_TARGET=$GLUON_TARGET GLUON_BRANCH=nightly GLUON_SITEDIR="$GLUON_SITEDIR" V=1 -j`nproc`
 
