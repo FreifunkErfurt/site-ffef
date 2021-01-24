f025c645d4d53f840e75d465f4e1d2d332e2b966
hipposen <hipposen@users.noreply.github.com>
2021-01-24 10:28:28 +0100

    Update install-dependencies.sh

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -8,6 +8,6 @@ apt update
 apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
 apt clean
 rm -rf /var/lib/apt/lists/*
-
+ln -s ../ gluon/site
 mkdir logs
 mkdir output
