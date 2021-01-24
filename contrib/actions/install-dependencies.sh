e050e558df88dd7f2efff88db30e4cfb1aefbdea
hipposen <hipposen@users.noreply.github.com>
2021-01-24 10:06:05 +0100

    Update install-dependencies.sh

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -8,3 +8,6 @@ apt update
 apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
 apt clean
 rm -rf /var/lib/apt/lists/*
+
+mkdir logs
+mkdir output
