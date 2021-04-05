80feb1a0aea1dc394b0e8ee662984456612c5219
hipposen <hipposen@users.noreply.github.com>
2021-04-05 02:20:44 +0200

    Sync to webserver (#25)
    * Update and rename build_single.yml to build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Update build_manual .yml
    
    * Create build_manual .yml
    
    * Update install-dependencies.sh
    
    * Update build_manual .yml
    
    * Update build_manual .yml

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -5,7 +5,7 @@ set -e
 cp /home/runner/work/site-ffef/site-ffef/contrib/actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
 apt update
-apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
+apt install git subversion build-essential python gawk unzip  zlib1g-dev  wget time
 apt clean
 rm -rf /var/lib/apt/lists/*
 ln -s ../ gluon/site
