65add40b1a7c5317210c6344a589750d93553767
hipposen <hipposen@users.noreply.github.com>
2021-04-05 10:07:14 +0200

    Update install-dependencies.sh

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -5,7 +5,7 @@ set -e
 cp /home/runner/work/site-ffef/site-ffef/contrib/actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
 apt update
-apt install git subversion build-essential python gawk unzip  zlib1g-dev  wget time
+apt install subversion build-essential python2.7 python3
 apt clean
 rm -rf /var/lib/apt/lists/*
 ln -s ../ gluon/site
