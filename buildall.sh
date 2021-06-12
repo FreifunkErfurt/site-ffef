6b6a6014ebb9fbf46268c40dcbd4b7be2caf77b7
GoliathLabs <git@xdfr.de>
2021-06-12 22:12:42 +0200

    Updated: gluon, configuration

--- buildall.sh
+++ buildall.sh
@@ -6,26 +6,27 @@ set -o pipefail
 branch=nightly
 
 TARGETS=(
-    ar71xx-generic
-    ar71xx-tiny
-    ar71xx-nand
-    ath79-generic
-    brcm2708-bcm2708
-    brcm2708-bcm2709
-    ipq40xx-generic
-    ipq806x-generic
-    lantiq-xrx200
-    lantiq-xway
-    mpc85xx-generic
-    mpc85xx-p1020
-    ramips-mt7620
-    ramips-mt7621
-    ramips-mt76x8
-    ramips-rt305x
-    sunxi-cortexa7
-    x86-generic
-    x86-geode
-    x86-64
+	ar71xx-generic
+	ar71xx-tiny
+	ar71xx-nand
+	brcm2708-bcm2708
+	brcm2708-bcm2709
+	mpc85xx-generic
+	mpc85cc-p1020
+	lantiq-xway
+	lantiq-xrx200
+	ath79-generic
+	ramips-mt7621
+	sunxi-cortexa7
+	x86-generic
+	x86-geode
+	x86-64
+	x86-legacy
+	ipq40xx-generic
+	ipq806x-generic
+	ramips-mt7620
+	ramips-mt76x8
+	ramips-rt305x
 )
 
 sitedir=$(readlink -f $(dirname $0))
