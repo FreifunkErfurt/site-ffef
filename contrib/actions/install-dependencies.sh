5909b0d291e40a071dce31ebf3b65f082cad1dd2
Bob Idle <102661087+bobidle@users.noreply.github.com>
2023-03-30 23:06:55 +0200

    Update apt dependencies (#41)

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -5,7 +5,7 @@ set -e
 cp /home/runner/work/site-ffef/site-ffef/contrib/actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
 apt update
-apt install subversion build-essential python2.7 python3
+apt install subversion build-essential libncurses-dev python2.7 python3 zlib1g-dev
 apt clean
 rm -rf /var/lib/apt/lists/*
 ln -s ../ gluon/site
@@ -15,5 +15,6 @@ chown runner:docker logs output
 pwd
 ls -l
 df -h
+dpkg -l
 echo "ENDE install-dependencies.sh "
 
