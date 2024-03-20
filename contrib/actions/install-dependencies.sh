97a640bf10948f528a5c62e07673cf6037257adf
Bob Idle <102661087+bobidle@users.noreply.github.com>
2024-03-20 07:37:19 +0100

    Update to Gluon v2022.1.4 (#47)

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -5,7 +5,7 @@ set -e
 cp /home/runner/work/site-ffef/site-ffef/contrib/actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
 apt update
-apt install subversion build-essential libncurses-dev python2.7 python3 zlib1g-dev
+apt install subversion build-essential libncurses-dev python2.7 python3 qemu-utils zlib1g-dev
 apt clean
 rm -rf /var/lib/apt/lists/*
 dpkg -l
