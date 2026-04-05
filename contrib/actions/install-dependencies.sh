17af5b832b62bdc1f829d826c90299efcc2ef512
hipposen <hipposen@users.noreply.github.com>
2026-04-05 12:03:54 +0200

    Refactor install-dependencies.sh to add more packages
    Updated the install script to include additional packages for development and build tools.

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -5,7 +5,33 @@ set -e
 cp /home/runner/work/site-ffef/site-ffef/contrib/actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
 apt update
-apt install subversion build-essential libncurses-dev python2.7 python3 qemu-utils zlib1g-dev
+apt install subversion \
+  build-essential \
+    ca-certificates \
+    clang \
+    ecdsautils \
+    file \
+    gawk \
+    git \
+    libelf-dev \
+    libncurses5-dev \
+    libnss-unknown \
+    libssl-dev \
+    llvm \
+    lua-check \
+    openssh-client \
+    python3 \
+    python3-dev \
+    python3-pyelftools \
+    python3-setuptools \
+    qemu-utils \
+    rsync \
+    shellcheck \
+    swig \
+    time \
+    unzip \
+    wget \
+    zlib1g-dev 
 apt clean
 rm -rf /var/lib/apt/lists/*
 dpkg -l
